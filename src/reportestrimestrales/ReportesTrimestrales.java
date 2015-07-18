a3d261e085cfcba05a1960ddb1718339cc391d88
Hugo Garcia <hugoallangm@gmail.com>
2015-07-18 15:59:00 -0600

    v  2.2.2
    Antes de version local. IPC

--- src/reportestrimestrales/ReportesTrimestrales.java
+++ src/reportestrimestrales/ReportesTrimestrales.java
@@ -7,7 +7,10 @@ package reportestrimestrales;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-
+import consultor.*;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author INE
@@ -18,30 +21,35 @@ public class ReportesTrimestrales {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            Conector c = new Conector("C:\\Users\\INE\\Downloads\\marzo.csv","C:\\Users\\INE\\Desktop\\CSV");
+        } catch (SQLException ex) {
+            Logger.getLogger(ReportesTrimestrales.class.getName()).log(Level.SEVERE, null, ex);
+        }
+            // TODO code application logic here
 //        MenuPrincipal menu = new MenuPrincipal();
 //        menu.setVisible(true);
 //        Calendar cal = Calendar.getInstance();
 //        System.out.println(new SimpleDateFormat("MMMM").format(cal.getTime()));
-        Vitales docu;
-        docu= new Vitales("Estadísticas Vitales", "Tercero", "2014","C:/Users/INE/Downloads/CSV_Vitales/CSV_Vitales");
-        docu.setRuta("C:/Users/INE/Documents/Vitales3/Vitales/");
-        docu.setTex("vitalesTercero2015");
-        docu.hacerPortada();
-        docu.preambulo();
-        docu.iniciarDocumento();
-        docu.hacerTitulo();
-        docu.juntaDirectiva();
-        docu.equipoYPresentacion();
-        docu.capitulo1();
-        docu.capitulo2();
-        docu.capitulo3();
-        docu.capitulo4();
-        docu.capitulo5();
-        docu.terminarDocumento();
-        docu.getRr().get().end();
-        docu.generarGraficas("presentacion");
-        docu.compilar(docu.getRr(),"C:/Users/INE/Documents/Vitales3/vitalesTercero2015.tex","T");
+//        Vitales docu;
+//        docu= new Vitales("Estadísticas Vitales", "Tercero", "2014","C:/Users/INE/Downloads/CSV_Vitales/CSV_Vitales");
+//        docu.setRuta("C:/Users/INE/Documents/Vitales3/Vitales/");
+//        docu.setTex("vitalesTercero2015");
+//        docu.hacerPortada();
+//        docu.preambulo();
+//        docu.iniciarDocumento();
+//        docu.hacerTitulo();
+//        docu.juntaDirectiva();
+//        docu.equipoYPresentacion();
+//        docu.capitulo1();
+//        docu.capitulo2();
+//        docu.capitulo3();
+//        docu.capitulo4();
+//        docu.capitulo5();
+//        docu.terminarDocumento();
+//        docu.getRr().get().end();
+//        docu.generarGraficas("presentacion");
+//        docu.compilar(docu.getRr(),"C:/Users/INE/Documents/Vitales3/vitalesTercero2015.tex","T");
         
 //        IPC docu;
 //        docu = new IPC("IPC", "Junio", "2015", "C:/Users/INE/Desktop/IPC/CSV_IPC");
