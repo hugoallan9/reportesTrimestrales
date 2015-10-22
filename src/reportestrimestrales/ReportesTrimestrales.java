93df799c404616e65687868e987cb1da05ba1816
Eiji De Paz <depazeiji@gmail.com>
2015-10-22 12:02:23 -0600

    probando merge

--- src/reportestrimestrales/ReportesTrimestrales.java
+++ src/reportestrimestrales/ReportesTrimestrales.java
@@ -2,6 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+comentario para el merge
  */
 package reportestrimestrales;
 import com.rabbitmq.client.*;
