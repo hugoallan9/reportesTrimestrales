ae0af7af3bfca375f9e001977ce0fd4b8b245041
Hugo Garcia <hugoallangm@gmail.com>
2015-12-18 20:29:26 -0600

    Arreglos a comercio

--- src/reportestrimestrales/ComercioExterior.java
+++ src/reportestrimestrales/ComercioExterior.java
@@ -703,33 +703,15 @@ public class ComercioExterior extends Documento{
     }
     
     protected void hacerPortada(){
-       String portada = "http://www.ine.gob.gt/ftparchivos/portadaVitales.pdf";
-       File file = new File(getRuta(),"portada.pdf");
-       URL url = null;
+       File source = new File("/home/ineservidor/Comercio/Caratula");
+        File dest = new File(getRuta());
         try {
-            url = new URL(portada);
-        } catch (MalformedURLException ex) {
-            Logger.getLogger(Documento.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        try {
-            FileUtils.copyURLToFile(url, file);
-        } catch (IOException ex) {
-            Logger.getLogger(Documento.class.getName()).log(Level.SEVERE, null, ex);
+            FileUtils.copyDirectory(source, dest);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         
-        String contraPortada = "http://www.ine.gob.gt/ftparchivos/contraportadaVitales.pdf";
-        File file1 = new File(getRuta(),"contraPortada.pdf");
-        try {
-            url = new URL(contraPortada);
-        } catch (MalformedURLException ex) {
-            Logger.getLogger(Documento.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        try {
-            FileUtils.copyURLToFile(url, file1);
-        } catch (IOException ex) {
-            Logger.getLogger(Documento.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
+  
         try {
             File p1 = new File(getRuta(),"caratula.tex");
             FileWriter escritora = new FileWriter(p1.getAbsolutePath(),false);
@@ -858,6 +840,16 @@ public class ComercioExterior extends Documento{
                 "\\cleardoublepage");
     }
     
+         @Override
+    protected void preambuloPresentacion(){
+        File source = new File("/home/ineservidor/Vitales/Presentacion");
+        File dest = new File(getRuta());
+        try {
+            FileUtils.copyDirectory(source, dest);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
 
