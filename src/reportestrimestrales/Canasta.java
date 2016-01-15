d7249afb1fd55fbceb109cc86d01560a68b19ef2
Hugo Garcia <hugoallangm@gmail.com>
2016-01-15 11:33:32 -0600

    Inicio de canasta basica

---
+++ src/reportestrimestrales/Canasta.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reportestrimestrales;
+
+/**
+ *
+ * @author hugo
+ */
+public class Canasta extends Documento{
+    
+    public Canasta(String titulo, String trimestre, String pYear){
+        super(titulo, trimestre, pYear);
+    }
+}
